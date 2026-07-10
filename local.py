293db938fe25e9174949461173c67dd4c0fddfbe
Sato0123 <rukajorudano@gmail.com>
2026-07-11 00:20:33 +0900

    feat: Inplement add cards

---
+++ local.py
@@ -0,0 +1,13 @@
+# Ankiの環境に依存する設定を辞書（dict）で管理します
+ANKI_CONFIG = {
+    "EN": {
+        "MODEL_NAME": "Basic",
+        "FRONT": "Front",  # 英語環境なら "Front"
+        "BACK": "Back",  # 英語環境なら "Back"
+    },
+    "JA": {
+        "MODEL_NAME": "基本",
+        "FRONT": "表面",  # 英語環境なら "Front"
+        "BACK": "裏面",  # 英語環境なら "Back"
+    },
+}
